bddfffaa9f02c95732a8e72b50183da0dbd26229
Richard Marko <srk@48.io>
2020-06-17 15:32:17 +0200

    Clean-up toplevel re-exports

--- src/Data/GCode.hs
+++ src/Data/GCode.hs
@@ -1,15 +1,23 @@
 {-| This module is an entry point to @gcodehs@ library
 
-This module re-exports all Data.GCode submodules.
+This module re-exports most of the "Data.GCode" submodules.
 
 -}
 
- {-# LANGUAGE OverloadedStrings #-}
-module Data.GCode (module X) where
-import Data.GCode.Eval as X
-import Data.GCode.Generate as X
-import Data.GCode.Types as X
-import Data.GCode.Parse as X
-import Data.GCode.Pretty as X
---import Data.GCode.RS274 as X
-import Data.GCode.Utils as X
+module Data.GCode (
+    module Data.GCode.Ann
+  , module Data.GCode.Eval
+  , module Data.GCode.Types
+  , module Data.GCode.Parse
+  , module Data.GCode.Pipes
+  , module Data.GCode.Pretty
+  , module Data.GCode.Utils
+  ) where
+
+import Data.GCode.Ann
+import Data.GCode.Eval
+import Data.GCode.Types
+import Data.GCode.Parse
+import Data.GCode.Pipes
+import Data.GCode.Pretty
+import Data.GCode.Utils
